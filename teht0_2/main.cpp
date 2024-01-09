d2e94efef46a56c65a06a162f94d34c6c192d4c2
Nilssoni <martikainen93@gmail.com>
2024-01-09 11:43:29 +0200

    teht0_2 lisätty

---
+++ teht0_2/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+void tarkista();
+int num;
+
+int main()
+{
+    num=27;
+    tarkista();
+
+    return 0;
+}
+
+void tarkista(int num) {
+    if (num > 0){
+        cout<<"positiivinen"<<endl;
+    }
+    else if (num < 0){
+        cout<<"negatiivinen"<<endl;
+    }
+    else {
+        cout<<"nolla"<<endl;
+    }
+}
