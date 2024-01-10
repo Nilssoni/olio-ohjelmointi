5b900c773d386041acf5a38975d538275be160de
Nilssoni <martikainen93@gmail.com>
2024-01-10 10:18:05 +0200

    teht0_8 valmis

--- teht0_8/main.cpp
+++ teht0_8/main.cpp
@@ -7,18 +7,16 @@ void sarja(int sluku);
 int main() {
     int sluku;
     cin >> sluku;
-
-    srand(sluku);
-
-
     sarja(sluku);
 
     return 0;
 }
 
 void sarja(int sluku) {
+    srand(sluku);
+    int satunnaisluku;
     for(int i = 0; i < 5; i++){
-        int satunnaisluku = rand() % 20;
+        satunnaisluku = rand() % 20;
         cout << satunnaisluku << endl;
     }
 }
