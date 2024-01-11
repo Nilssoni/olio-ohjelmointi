0008b26aa0cdad5592def2bd8dc2432c038787e0
Nilssoni <martikainen93@gmail.com>
2024-01-11 16:49:07 +0200

    testi

---
+++ Harjoitustehtävät/Viikko 1/teht0_8/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+void sarja(int);
+
+int main() {
+    int sluku;
+    cin >> sluku;
+    sarja(sluku);
+
+    return 0;
+}
+
+void sarja(int sluku) {
+    srand(sluku);
+    int satunnaisluku;
+    for(int i = 0; i < 5; i++){
+        satunnaisluku = rand() % 20;
+        cout << satunnaisluku << endl;
+    }
+}
