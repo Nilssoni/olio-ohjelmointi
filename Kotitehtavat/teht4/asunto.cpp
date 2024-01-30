5fc21bd96435f658e74e9f522544f696e83a76a8
Nilssoni <martikainen93@gmail.com>
2024-01-30 11:44:04 +0200

    teht4_1 valmis

---
+++ Kotitehtavat/teht4/asunto.cpp
@@ -0,0 +1,19 @@
+#include "asunto.h"
+
+Asunto::Asunto()
+{
+    cout << "asunto luotu" << endl;
+}
+
+void Asunto::maarita(int a, int b)
+{
+    asukasMaara = a;
+    neliot = b;
+    cout << "Asunto maaritetty asukkaita = " << asukasMaara << " nelioita = " << neliot << endl;
+}
+
+double Asunto::laskeKulutus(double hinta)
+{
+    hinta = asukasMaara * neliot;
+    cout << "Asunnon kulutus, kun hinta = " << hinta <<  endl;
+}
---
+++ Kotitehtavat/teht4/main.cpp
@@ -0,0 +1,13 @@
+#include "asunto.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    Asunto tiedot;
+    tiedot.maarita(2, 100);
+    tiedot.laskeKulutus(0);
+    return 0;
+}
